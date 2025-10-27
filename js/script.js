c478c35d40e549ce57a73e39f6a3b65afc61e697
shakirhussaindev <shakirhusain1414@gmail.com>
2025-10-28 00:46:19 +0600

    active edit and update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ let post = document.querySelector(".post");
 let name0 = document.querySelector(".name0");
 let caption = document.querySelector(".caption");
 let postbtn = document.querySelector(".postbtn");
+let update=document.querySelector(".update")
+
+let EditStor;
+
+
+
 
 let arr = [];
 
@@ -40,13 +46,36 @@ postbtn.addEventListener("click", () => {
   
 });
 
+
+update.addEventListener("click",()=>{
+  
+
+  arr[EditStor].name0=name0.value
+  arr[EditStor].caption=caption.value
+  
+  post.innerHTML=""
+
+  show()
+
+
+  update.style="display:none"
+  postbtn.style="display:inline-block"
+
+    name0.value = "";
+    caption.value = "";
+  
+})
+
+
+
+
 function show() {
   arr.map((item) => {
     post.innerHTML += `<div class="card" style="width: 18rem">
     <div class="card-body">
     <h5 class="card-title">${item.name0}</h5>
     <p class="card-text">${item.caption}</p>
-    <button class="btn btn-primary">Edit</button>
+    <button class="btn btn-primary edit">Edit</button>
     <button class="btn btn-danger delete">Delete</button>
     </div>
     </div>`;
@@ -62,8 +91,27 @@ function show() {
       show()
       
     })
+
+
   })
-  
+  let edit=document.querySelectorAll(".edit")
+  let editBtn=Array.from(edit)
+  editBtn.map((item,index)=>{
+    item.addEventListener("click",()=>{
+      // console.log(arr[index].name0);
+
+      name0.value=arr[index].name0
+      caption.value=arr[index].caption
+
+      EditStor=index
+
+      update.style="display:inline-block"
+      postbtn.style="display:none"
+
+      
+      
+    })
+  })
 
 }
 
